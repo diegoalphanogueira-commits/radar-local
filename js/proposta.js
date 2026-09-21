531f7e54d8ac4b6c3ed9e727bd2180a16e77f2c9
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-20 21:26:06 -0300

    Atualiza versão do loader para benchmark persistente

--- js/proposta.js
+++ js/proposta.js
@@ -6,7 +6,7 @@
 ========================================================= */
 
 (() => {
-  const VERSION = "20260920-benchmark-v1";
+  const VERSION = "20260920-benchmark-v2";
 
   const loadScript = src => new Promise((resolve, reject) => {
     const script = document.createElement("script");
