eb7592d5703f794c1239dd2adeb9db4983ab0432
diegoalphanogueira-commits <diegoalphanogueira@gmail.com>
2026-09-22 19:15:19 -0300

    chore: refresh V1 assets for RC12

--- chrome-extension/radar-maps-collector/radar-v1-bootstrap-v2.js
+++ chrome-extension/radar-maps-collector/radar-v1-bootstrap-v2.js
@@ -3,7 +3,7 @@
 
   if (!/\/prospeccao(?:\.html)?\/?$/i.test(window.location.pathname)) return;
   const origin = window.location.origin;
-  const version = "20260922-v1rc10";
+  const version = "20260922-v1rc12";
 
   function injectCss(id, href) {
     if (document.getElementById(id)) return;
